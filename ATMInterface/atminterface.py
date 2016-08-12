d0f9e0078fa0a256b16dad58a3e594562cf34b51
Karlo Grlić <karlogrlicweb@gmail.com>
2016-08-12 03:01:48 +0200

    Fully functional ATM client, authentication server and mobile token. Lot of bugs, but it works!

--- ATMInterface/atminterface.py
+++ ATMInterface/atminterface.py
@@ -2,10 +2,12 @@
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, GObject, Gdk, GLib
 from thread import *
+import time
 import socket
 import subprocess
+import threading
 from sendfile import sendfile
 
 #Informacije o uredaju
@@ -26,11 +28,11 @@ class GUI:
         while True:
             data = client.recv(1024)
             if not data: break
-            key, message = data.split(":")
-            if key == "ok":
+            components = data.split(":")
+            if components[0] == "ok":
                 break
             else:
-                return (key, message)
+                return components
 
     def sendFaceImages(self):
         for i in range(0, 5):
@@ -47,9 +49,40 @@ class GUI:
             self.waitForResponse()
             self.statusLabel.set_text("Uzorci se provjeravaju")
 
+    def waitForTokenApproval(self):
+        response = self.waitForResponse()[1]
+        self.infoWindow.hide()
+        self.infoWindow2.show()
+        if response == "ok":
+            self.infoTitle2.set_text("Transakcija odobrena")
+            self.infoText2.set_text("Transakcija je uspjesno obavljena!")
+        else:
+            self.infoTitle2.set_text("Transakcija odbijena")
+            self.infoText2.set_text("Transakcija nije uspjesno obavljena!")
+
     def sendPIN(self):
         client.send("pin:" + str(self.inputPin.get_text()))
-        self.waitForResponse()
+        key, pinmessage, authmessage, token = self.waitForResponse()
+        if key == "ath":
+            if pinmessage == "pin;ok":
+                if authmessage == "fce;ok":
+                    self.infoWindow.show()
+                    self.infoTitle.set_text("Transakcija obavljena")
+                    self.infoText.set_text("Transakcija je uspjesno obavljena!")
+                elif authmessage == "fce;fls":
+                    self.infoWindow.show()
+                    self.infoTitle.set_text("Transakcija neuspjesna")
+                    self.infoText.set_text("Token za odobrenje: " + token.split(";")[1])
+                    #GLib.idle_add(self.waitForTokenApproval, 0)
+                    #time.sleep(0.2)
+                    #start_new_thread(self.waitForTokenApproval, (0,))
+                    thread = threading.Thread(target=self.waitForTokenApproval)
+                    thread.daemon = True
+                    thread.start()
+            else:
+                self.infoWindow.show()
+                self.infoTitle.set_text("Transakcija neuspjesna")
+                self.infoText.set_text("PIN nije valjan")
 
     def sendAccountNumber(self):
         client.send("acc:" + str(self.getActiveComboItem()))
@@ -88,6 +121,17 @@ class GUI:
             return False
         else:
             return True
+
+    def on_infoWindow2_activate_default(self, data=None):
+        self.response = self.infoWindow2.show()
+        self.infoWindow2.hide()
+
+    def on_hideInfoWindow2_clicked(self, object):
+        self.infoWindow2.hide()
+
+    def on_infoWindow_activate_default(self, data=None):
+        self.response = self.infoWindow.show()
+        self.infoWindow.hide()
 
     def on_gtk_about_activate(self, menuitem, data=None):
         self.response = self.aboutDialog.run()
@@ -140,7 +184,7 @@ class GUI:
     def on_btnConfirm_clicked(self, object):
         if self.inputPin.get_text_length() == 4:
             self.sendPIN()
-        client.close()
+        #client.close()
 
 
     def on_gtk_quit_activate(self, menuitem, data=None):
@@ -156,6 +200,7 @@ class GUI:
         self.mainWindow = self.builder.get_object("mainWindow")
         self.aboutDialog = self.builder.get_object("aboutDialog")
         self.infoWindow = self.builder.get_object("infoWindow")
+        self.infoWindow2 = self.builder.get_object("infoWindow2")
 
         #definiranje elemenata gui-a
         self.statusLabel = self.builder.get_object("statusLabel")
@@ -174,6 +219,14 @@ class GUI:
         self.btnReset = self.builder.get_object("btnReset")
         self.btnConfirm = self.builder.get_object("btnConfirm")
 
+        #infowindow
+        self.infoTitle = self.builder.get_object("infoTitle")
+        self.infoText = self.builder.get_object("infoText")
+
+        #infowindow2
+        self.infoTitle2 = self.builder.get_object("infoTitle2")
+        self.infoText2 = self.builder.get_object("infoText2")
+
         #Popunjavanje comboboxa
         self.store = self.builder.get_object("accNums")
         self.cbxAccountNum.set_model(self.store)
@@ -187,7 +240,6 @@ class GUI:
         #Prikazivanje glavnog prozora
         self.mainWindow.show()
 
-
 if __name__ == "__main__":
     #Spajanje na autentikacijski server
     try:
@@ -195,5 +247,6 @@ if __name__ == "__main__":
     except socket.error as msg:
         print "Nemogucnost spajanja na autentikacijski server. Error: " + str(msg[0]) + " Poruka: " + msg[1]
         exit()
+    GObject.threads_init()
     main = GUI()
     Gtk.main()
--- AuthenticationServer/authserver.py
+++ AuthenticationServer/authserver.py
@@ -5,7 +5,9 @@
 import socket
 from thread import *
 import cv2, os
+import time
 import numpy as np
+import hashlib
 from PIL import Image
 
 HOST = ""
@@ -14,7 +16,6 @@ PORT = 8888
 userList = []
 interfaceList = []
 transactionList = []
-allowedMessages = ['ifc', 'usr', 'ftp']
 
 
 class Transaction(object):
@@ -23,34 +24,65 @@ class Transaction(object):
     transactionCode = None
     panNumber = None
     mobileToken = None
-    approved = None
+    faceDict = None
 
 
 class User(object):
+    connection = None
     ipAddress = None
     port = None
     phoneKey = None
     phoneId = None
+    panNumber = '874300775423885'
 
-    def __init__(self, identification, ipaddres, port):
+    def __init__(self, identification, ipaddres, port, connection):
         self.phoneKey = identification.split(";")[0]
         self.phoneId = identification.split(";")[1]
         self.ipAddress = ipaddres
         self.port = port
-
+        self.connection = connection
 
 class Interface:
     interfaceId = None
     ipAddress = None
     port = None
+    connection = None
 
-    def __init__(self, interfaceid, ipaddres, port):
+    def __init__(self, interfaceid, ipaddres, port, connection):
         self.interfaceId = interfaceid
         self.ipAddress = ipaddres
         self.port = port
+        self.connection = connection
 
 class FaceRecognitionTrainer:
-    a = None
+    recognizer = cv2.face.createLBPHFaceRecognizer()
+    imageList = []
+    labelList = []
+    panNumber = None
+    imagePath = None
+    newImageFile = None
+    dataFile = None
+
+    def __init__(self, panNumber, newImageFile):
+        self.panNumber = panNumber
+        self.dataFile = "Users/" + str(panNumber) + "/" + str(panNumber) + ".yml"
+        self.imagePath = "Users/" + str(panNumber) + "/Samples"
+        self.newImageFile = "Auth/" + str(panNumber) + "/" + newImageFile
+
+    def train(self):
+        print "Treniranje podataka  ..."
+        '''
+        os.rename(self.newImageFile, "Users/" + str(self.panNumber) + "/Samples/" + str(int(time.time())) + '.jpg')
+        for faceImageFile in os.listdir("Users/" + str(self.panNumber) + "/Samples/"):
+            faceImage = cv2.imread("Users/" + str(self.panNumber) + "/Samples/" + faceImageFile, 0)
+            self.imageList.append(faceImage)
+            self.labelList.append(hash(self.panNumber))
+        self.recognizer.train(self.imageList, np.array(self.labelList))
+        '''
+        faceImage = cv2.imread(self.newImageFile, 0)
+        self.recognizer.update([faceImage], np.array([hash(self.panNumber)]))
+        self.recognizer.save("Users/" + str(self.panNumber) + "/" + str(self.panNumber) + ".yml")
+        print "Treniranje podataka dovrseno ..."
 
 class FaceRecognizer:
     cascadePath = "haarcascade_frontalface_default.xml"
@@ -67,6 +99,15 @@ class FaceRecognizer:
             return distance
 
 
+# Metoda za uklanjanje podataka nakon transakcije
+def cleanData(transaction):
+    if transaction != None:
+        interfaceList.remove(transaction.interface)
+        if transaction.user in userList:
+            userList.remove(transaction.user)
+        transactionList.remove(transaction)
+
+
 # Metoda za prihvacanje klijenta
 def connectionThread(connection, addr):
 
@@ -80,16 +121,44 @@ def connectionThread(connection, addr):
         key, message = data.split(":")
 
         if key == "ifc":
-            interface = Interface(message, addr[0], addr[1])
+            interface = Interface(message, addr[0], addr[1], connection)
             interfaceList.append(interface)
-            connection.send(unicode("ok:ok\n"))
+            connection.send(unicode("ok:ok"))
 
         elif key == "usr":
-            user = User(message, addr[0], addr[1])
+            user = User(message, addr[0], addr[1], connection)
             userList.append(user)
-            connection.send(unicode("idle:idle\n"))
+            transaction = next((x for x in transactionList if x.panNumber == user.panNumber), None)
+            if transaction == None:
+                connection.send(unicode("idl:idl"))
+            elif transaction.mobileToken != None and len(transaction.mobileToken) == 8:
+                connection.send(unicode("tkn:" + transaction.mobileToken + "\n"))
+            else:
+                connection.send(unicode("idl:idl"))
+
+        elif key == "ok" or key == "no":
+            transaction = next((x for x in transactionList if x.mobileToken == message[:8]), None)
+            if transaction == None:
+                connection.send(unicode("idl:idl"))
+            elif key == "ok":
+                transaction.interface.connection.send("tkn:ok")
+                leastDistance = min(transaction.faceDict.itervalues())
+                if leastDistance <= 50 and leastDistance != None:
+                    leastDistanceKeys = transaction.faceDict.keys()[transaction.faceDict.values().index(leastDistance)]
+                    faceRecognitionTrainer = FaceRecognitionTrainer(transaction.panNumber, leastDistanceKeys)
+                    faceRecognitionTrainer.train()
+            elif key == "no":
+                transaction.interface.connection.send("tkn:no")
+            time.sleep(1)
+            cleanData(transaction)
+
 
         elif key == "acc":
+            #uklanjanje starih podataka
+            transaction = next((x for x in transactionList if x.panNumber == message), None)
+            if transaction != None:
+                cleanData(transaction)
+            #novi podaci
             transaction = Transaction()
             transaction.panNumber = message
             transaction.interface = next((x for x in interfaceList if x.ipAddress == addr[0] and x.port == addr[1]), None)
@@ -97,19 +166,41 @@ def connectionThread(connection, addr):
 
             if not os.path.exists("Auth/" + str(message)):
                 os.makedirs("Auth/" + str(message))
-            connection.send(unicode("ok:ok\n"))
+            connection.send(unicode("ok:ok"))
 
         elif key == "pin":
             transaction = next((x for x in transactionList if x.interface.ipAddress == addr[0] and x.interface.port == addr[1]), None)
+
+            # Prepoznavanje
             faceRecognizer = FaceRecognizer(transaction.panNumber)
+            faceDict = {}
             for faceImageFile in os.listdir("Auth/" + str(transaction.panNumber)):
-                faceImage = cv2.imread("Auth/" + str(transaction.panNumber) + "/" + faceImageFile,0)
-                print faceRecognizer.recognize(faceImage)
-            connection.send(unicode("ok:ok\n"))
+                faceImage = cv2.imread("Auth/" + str(transaction.panNumber) + "/" + faceImageFile, 0)
+                faceDistance = faceRecognizer.recognize(faceImage)
+                if faceDistance == None:
+                    faceDict[faceImageFile] = 999
+                else:
+                    faceDict[faceImageFile] = faceDistance
+            transaction.faceDict = faceDict
+
+            #Ako je prepoznavanje uspjesno onda uzmi najpovoljniju fotografiju i pridodaj ostalim uzorcima
+            leastDistance = min(transaction.faceDict.itervalues())
+            if leastDistance <= 30 and leastDistance != None:
+                print "Uzorak prihvacen, udaljenost =", leastDistance
+                connection.send(unicode("ath:pin;ok:fce;ok:tkn;none"))
+                leastDistanceKeys = transaction.faceDict.keys()[transaction.faceDict.values().index(leastDistance)]
+                faceRecognitionTrainer = FaceRecognitionTrainer(transaction.panNumber, leastDistanceKeys)
+                faceRecognitionTrainer.train()
+                cleanData(transaction)
+            else:
+                print "Uzorak odbijen, udaljenost =", leastDistance
+                print "Poslan zahtijev za token .."
+                transaction.mobileToken = hashlib.sha1((transaction.panNumber + transaction.interface.ipAddress + str(int(time.time()))).encode("UTF-8")).hexdigest()[:8]
+                connection.send(unicode("ath:pin;ok:fce;fls:tkn;" + transaction.mobileToken))
 
 
         elif key == "ftp":
-            connection.send(unicode("ok:ok\n"))
+            connection.send(unicode("ok:ok"))
             print "Primam uzorke lica ..."
             transaction = next((x for x in transactionList if x.interface.ipAddress == addr[0] and x.interface.port == addr[1]), None)
             imageFile = open("Auth/" + str(transaction.panNumber) + "/" + str(message) + '.jpg', 'w')
@@ -119,11 +210,10 @@ def connectionThread(connection, addr):
                 if not imageBytes or imageBytes == "EOF": break
                 if imageBytes[-3:] == "EOF":
                     imageFile.write(imageBytes[:-3])
-                    connection.send(unicode("ok:ok\n"))
+                    connection.send(unicode("ok:ok"))
                     break
 
                 imageFile.write(imageBytes)
-
             imageFile.close()
 
     connection.close()
@@ -141,6 +231,7 @@ def startServer(host, port):
 
     print "IP Servera: " + localIpAddress
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # serverSocket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
     try:
